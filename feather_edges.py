7e5685036309327a446dee36c299ad7ffb7ce15c
sunrise <sunrisewng@gmail.com>
2023-11-28 15:40:08 +0100

    deleted a bunch of results, will rerun with slightly different data later, updated some helper scripts

--- feather_edges.py
+++ feather_edges.py
@@ -4,29 +4,37 @@ import sys
 
 ifilename = sys.argv[1]
 feather_size = int(sys.argv[2])
-ofilename = sys.argv[3]
+feather_end = int(sys.argv[3])
+ofilename = sys.argv[4]
 
 f = fits.open(ifilename)
 
-for y, row in enumerate(f[0].data):
-	for x, pixel in enumerate(row):
-		y_fixed = min(y, len(f[0].data) - y)
-		x_fixed = min(x, len(row) - x)
+data = f[0].data
+while len(data.shape) > 2:
+    data = data[0]
 
-		tx = x_fixed / feather_size
-		ctx = 3 * tx * tx - 2 * tx * tx * tx
-		ty = y_fixed / feather_size
-		cty = 3 * ty * ty - 2 * ty * ty * ty
+for y, row in enumerate(data):
+    for x, pixel in enumerate(row):
+        y_fixed = min(y, len(data) - y) - feather_end
+        x_fixed = min(x, len(row) - x) - feather_end
 
-		feather_x = 1 if x_fixed > feather_size else ctx
-		feather_y = 1 if y_fixed > feather_size else cty
+        feather = 0
 
-		feather = min(feather_x, feather_y)
+        if y_fixed >= 0 and x_fixed >= 0:
+            tx = x_fixed / feather_size
+            ctx = 3 * tx * tx - 2 * tx * tx * tx
+            ty = y_fixed / feather_size
+            cty = 3 * ty * ty - 2 * ty * ty * ty
 
-		#f[0].data[x, y] *= feather
+            feather_x = 1 if x_fixed > feather_size else ctx
+            feather_y = 1 if y_fixed > feather_size else cty
+
+            feather = min(feather_x, feather_y)
+
+        data[x, y] *= feather
 
 newf = fits.PrimaryHDU()
-newf.data =[[f[0].data]]
+newf.data =[[data]]
 newf.header = f[0].header
 
 hdulist = fits.HDUList([newf])
